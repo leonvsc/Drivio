8ab7069363d20999fbd7feeee6e215799dd0cbf1
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2022-10-16 17:55:22 +0200

    Removed unused files

--- src/main/java/nl/avans/drivio/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-//package nl.avans.drivio.repository;
-//
-//import nl.avans.drivio.model.Car;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface CarRepository extends JpaRepository<Car, Integer> {
-//}
