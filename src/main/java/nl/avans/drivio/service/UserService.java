ec9083e24f8e1f94ab82d83d487e2a384f3d3ec8
Huseyin Tufekci <h.tufekci@student.avans.nl>
2022-10-21 18:08:45 +0200

    Business logic user added

--- src/main/java/nl/avans/drivio/service/UserService.java
+++ src/main/java/nl/avans/drivio/service/UserService.java
@@ -4,7 +4,11 @@ package nl.avans.drivio.service;
 import nl.avans.drivio.model.User;
 import nl.avans.drivio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -22,15 +26,41 @@ public class UserService {
         return (List<User>) userRepository.findAll();
     }
 
-    public Optional<User> getById(int id) {
-        return userRepository.findById(id);
+    public User getUserById(int userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new IllegalArgumentException("No user found with given id["+ userId +"]");
+        }
     }
 
     public void add(User user) {
         userRepository.save(user);
     }
 
-    public void delete(User user) {
-        userRepository.delete(user);
+    public User updateUser(User user, int userId) {
+
+        // check whether a user with given id exists in the DB or not
+        User existingUser = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("No user found with given id["+ userId +"]"));
+
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setCity(user.getCity());
+        existingUser.setPhone(user.getPhone());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPasswords(user.getPasswords());
+
+        // save existing user to the Database
+        userRepository.save(existingUser);
+        return existingUser;
+    }
+
+    public void deleteUser(int userId) {
+
+        // check whether a user exists in the database. if not, throw exception.
+        userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("No user found with given id["+ userId +"]"));
+        userRepository.deleteById(userId);
     }
 }
