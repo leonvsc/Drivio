3943ca7420f302bd5b429a297acfac2caba84e05
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2022-10-22 21:02:15 +0200

    Added two tests

---
+++ src/test/java/nl/avans/drivio/service/FuelCarTest.java
@@ -0,0 +1,31 @@
+package nl.avans.drivio.service;
+
+import nl.avans.drivio.model.FuelCar;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class FuelCarTest {
+    private FuelCar fuelCar;
+
+    @BeforeEach
+    void beforeEach() {
+        fuelCar = mock(FuelCar.class);
+    }
+
+
+    @Test
+    void getCarIdTest() {
+        int notExisting;
+        if (fuelCar.getCarId() == 0) {
+            notExisting = 1;
+        }
+        else {
+            notExisting = 2;
+        }
+        boolean testing = notExisting == 1;
+        assertTrue(testing);
+    }
+}
