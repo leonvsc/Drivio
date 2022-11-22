6619cf77a533b16c7bbf420a28981e0aa91ba8fd
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2022-11-22 21:39:28 +0100

    Added WebConfig to the code for Angular app

--- src/main/java/nl/avans/drivio/DrivioApplication.java
+++ src/main/java/nl/avans/drivio/DrivioApplication.java
@@ -2,10 +2,14 @@ package nl.avans.drivio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DrivioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DrivioApplication.class, args);
+
+
 	}
 }
